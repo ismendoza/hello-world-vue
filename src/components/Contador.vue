f85ae88fd275f3ea212e5b9c8962bc97ad9bc782
Ismael <ismendoza@outlook.com>
2021-10-19 23:12:39 -0600

    Componente contador de clicks

---
+++ src/components/Contador.vue
@@ -0,0 +1,27 @@
+<template>
+    <button @click="number++"> Click here </button>
+    <p>{{ number }}</p>
+</template>
+
+<script>
+export default {
+    name: 'Contador',
+    data(){
+        return{
+            number: 0
+        }
+    }
+
+
+}
+</script>
+
+<style scoped>
+button{
+    color: steelblue;
+    font-size: 20px;
+}
+p{
+    font-size: 25px;
+}
+</style>
