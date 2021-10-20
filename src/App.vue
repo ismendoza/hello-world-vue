fa2867e330e3dbc7157ae72d9b7391bb4bd69610
Ismael <ismendoza@outlook.com>
2021-10-19 23:13:30 -0600

    Se importan los componentes y se agregan en el template

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HolaMundo mensaje="Bienvenido a vuejs" autor="Ismael Mendoza"/>
+  <Contador />
+  <Methods />
 </template>
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HolaMundo from './components/HolaMundo.vue'
+import Contador from './components/Contador.vue'
+import Methods from './components/Methods.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, HolaMundo, Contador, Methods
   }
 }
 </script>
